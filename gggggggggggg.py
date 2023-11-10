e04e6e0eda510fdce62106ac62f69a22dd19c4e3
dharani134 <150414989+dharani134@users.noreply.github.com>
2023-11-10 12:33:23 +0530

    Update gggggggggggg.py-10/11/23

--- gggggggggggg.py
+++ gggggggggggg.py
@@ -9,7 +9,7 @@ num = 29
 flag = False
 
 if num == 1:
-    print(num, "not a prime number")
+    print(num, "is not a prime number")
 elif num > 1:
     # check for factors
     for i in range(2, num):
@@ -21,6 +21,6 @@ elif num > 1:
 
     # check if flag is True
     if flag:
-        print(num, "not a prime number")
+        print(num, " is not a prime number")
     else:
-        print(num, "prime number")
+        print(num, "is a prime number")
